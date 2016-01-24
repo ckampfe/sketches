7e7db1aeda46cf0ecf52e6ae141befb287c4b93d
Clark Kampfe <clark@bellycard.com>
2016-01-24 13:35:40 -0600

    add cljs to project.clj

--- project.clj
+++ project.clj
@@ -4,4 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [quil "2.3.0"]])
+                 [quil "2.3.0"]
+                 [org.clojure/clojurescript "1.7.228"]]
+
+  :plugins [[lein-cljsbuild "1.1.1"]]
+  :hooks [leiningen.cljsbuild]
+  :cljsbuild {:builds [{:source-paths ["src"]
+                        :compiler {:output-to "js/main.js"
+                                   :output-dir "out"
+                                   :main "sketches.core"
+                                   :optimizations :none
+                                   :pretty-print true}}]})
