9236ae2ac602f37b7cfac246ffcd0c6065961445
Clark Kampfe <clark@bellycard.com>
2016-01-30 22:11:39 -0600

    project.clj license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject sketches "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "sketches"
+  :url "https://github.com/ckampfe/sketches"
+  :license {:name "GNU AGPL"
+            :url "https://www.gnu.org/licenses/agpl.txt"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [quil "2.3.0"]
                  [org.clojure/clojurescript "1.7.228"]]
